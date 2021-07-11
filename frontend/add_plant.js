78ba34cceda8da6e214328c0bf2beb238b37d2df
Gabriel Negreiros Lima <gabrielnegreiroslima@gmail.com>
2021-07-11 14:07:10 -0300

    Fix star rating that was not correctly unselecting the stars

--- frontend/add_plant.js
+++ frontend/add_plant.js
@@ -4,7 +4,22 @@ const stars = document.querySelectorAll(".fa.fa-star");
 stars.forEach( el => {
     el.addEventListener("click", e => {
         const starEl = e.currentTarget;
-        starEl.classList.toggle("checked");
+        const currentStarIdNumber = starEl.id[4];
+        const numberOfStars = 5;
+        let previousStar;
+        
+        if (starEl.classList.contains("checked")) {
+            for (let index = numberOfStars; index >= currentStarIdNumber; index--) {
+                previousStar = document.querySelector(`#star${index}`);
+                previousStar.classList.remove("checked");
+            }
+            return;
+        }
+
+        for (let index = 1; index <= currentStarIdNumber; index++) {
+            previousStar = document.querySelector(`#star${index}`);
+            previousStar.classList.add("checked");
+        }
     });
 });
 
