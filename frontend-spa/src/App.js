b6b51cb16794e2c6304a2e94ae5298cab2fe089c
Gabriel Negreiros Lima <gabrielnegreiroslima@gmail.com>
2021-08-21 17:04:12 -0300

    Add create react app

---
+++ frontend-spa/src/App.js
@@ -0,0 +1,13 @@
+import Header from './components/Header.js'
+
+function App() {
+  
+  return (
+    <div className="App">
+      Cole {potato}! 
+      <Header />
+    </div>
+  );
+}
+
+export default App;
