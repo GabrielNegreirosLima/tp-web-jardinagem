ab1a637f0850c3deccda495a577d9878f409c09f
yansirianni <yanvictorsa@yahoo.com.br>
2021-09-05 15:23:49 -0300

    Create public fav plants endpoint

--- src/routes/fav.js
+++ src/routes/fav.js
@@ -27,4 +27,32 @@ router.get('/:id', async (req, res, next) => {
     }
 })
 
+router.get('/public/:id', async (req, res, next) => {
+    try {
+        const [user] = await db.execute({
+            sql: `SELECT * FROM Users
+                  WHERE Users.id=?`,
+            values: [req.params.id]
+        })
+
+        const [fav] = await db.execute({
+            sql: `SELECT *
+                  FROM Plants
+                  LEFT OUTER JOIN FavPlants
+                    ON FavPlants.plant=Plants.id
+                  WHERE FavPlants.user=?`,
+            values: [req.params.id],
+            nestTables: true
+          })             
+
+        res.format({
+            html: () => res.render('fav_plants_public', {user : user[0], fav})
+        })
+    } catch (error) {
+        console.error(error)
+        error.friendlyMessage = 'Não foi possível recuperar a lista de plantas favoritas'
+        next(error)
+    }
+})
+
 export default router
